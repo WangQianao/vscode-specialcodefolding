e3aa4294839dd49b510c88df2bae19d83295c99e
WangQianao <2423114282@qq.com>
2022-11-06 19:17:40 +0800

    可以读取.json的语法文件了

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,21 @@
 
+import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
-// import * as fs from 'fs';
-// import * as path from 'path';
-// import * as vsctm from 'vscode-textmate';
-// import * as oniguruma from 'vscode-oniguruma';
-const fs = require('fs');
+import * as fs from 'fs';
+import * as vsctm from 'vscode-textmate';
+//const fs = require('fs');
 const path = require('path');
-const vsctm = require('vscode-textmate');
+//const vsctm = require('vscode-textmate');
 const oniguruma = require('vscode-oniguruma');
 
 
-function readFile(path: string): Promise<Buffer> {
-	return new Promise((resolve, reject) => {
-		fs.readFile(path, (error: NodeJS.ErrnoException | null, data: Buffer) => error ? reject(error) : resolve(data));
-	});
-}
-function tokenText(extensionUri:string) {
+// function readFile(path: string): Promise<Buffer> {
+// 	return new Promise((resolve, reject) => {
+// 		fs.readFile(path, (error: NodeJS.ErrnoException | null, data: Buffer) => error ? reject(error) : resolve(data));
+// 	});
+// }
+async function tokenText(extensionUri:string,originText?:string) {
+	
 	const wasmBin = fs.readFileSync(path.join(extensionUri, '/src/res/oniguruma/onig.wasm')).buffer;
 	const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
 		return {
@@ -26,20 +26,28 @@ function tokenText(extensionUri:string) {
 	// Create a registry that can create a grammar from a scope name.
 	const registry = new vsctm.Registry({
 		onigLib: vscodeOnigurumaLib,
-		loadGrammar: (scopeName:string) => {
-			// https://github.com/textmate/javascript.tmbundle/blob/master/Syntaxes/JavaScript.plist
-			return readFile(path.join(extensionUri, '/src/res/grammar/JavaScript.plist')).then(data => vsctm.parseRawGrammar(data.toString()));
+		
+		loadGrammar: async (scopeName:string) => {
+			if (scopeName === 'source.java') {
+				
+				// https://github.com/textmate/javascript.tmbundle/blob/master/Syntaxes/JavaScript.plist
+				const grammarFilePath=path.join(extensionUri, '/src/res/grammar/java.tmLanguage.json');
+				const response = await new Promise<string>((resolve, reject) => {
+					fs.readFile(grammarFilePath, (e, v) => e ? reject(e) : resolve(v.toString()));
+				});
+				const g = vsctm.parseRawGrammar(response, path.resolve(grammarFilePath));
+				return g;
+			}
+			console.log(`Unknown scope name: ${scopeName}`);
+			return null;
 
 		}
 	});
 	// Load the JavaScript grammar and any other grammars included by it async.
-	registry.loadGrammar('source.js').then((grammar:any) => {
-		const text = [
-			`function sayHello(name) {`,
-			`\treturn "Hello, " + name;`,
-			`}`
-		];
-		if (grammar !== null) {
+	const grammar = await registry.loadGrammar('source.java');
+	
+		const text=originText?.split('\r\n');
+		if (grammar!==null&&text!==undefined) {
 			let ruleStack = vsctm.INITIAL;
 			for (let i = 0; i < text.length; i++) {
 				const line = text[i];
@@ -55,8 +63,6 @@ function tokenText(extensionUri:string) {
 				ruleStack = lineTokens.ruleStack;
 			}
 		}
-
-	});
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -67,16 +73,17 @@ export function activate(context: vscode.ExtensionContext) {
 			"opacity": "0"
 		}
 	);
-	tokenText(context.extensionUri.path.substring(1));
-	context.subscriptions.push(vscode.commands.registerCommand('codefoldingbasedonconerns.fold', () => {
+	
+	context.subscriptions.push(vscode.commands.registerCommand('codefoldingbasedonconerns.fold', async () => {
 
 		const editor = vscode.window.activeTextEditor;
-		//const text=editor?.document.getText(editor.selection);
+		const text=editor?.document.getText(editor.selection);
+		
+		await tokenText(context.extensionUri.path.substring(1),text);
 		foldDecorationType = vscode.window.createTextEditorDecorationType(
 			{
 				"opacity": "0"
 			});
-
 		editor?.setDecorations(foldDecorationType, [editor.selection]);
 
 	}));
